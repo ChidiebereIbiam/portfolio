b329755e9c9b78cf378a5459f4b247734a72af1e
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-12-02 10:46:26 +0100

    Removed preloader

--- static/js/theme.js
+++ static/js/theme.js
@@ -3,12 +3,7 @@
 (function ($) {
 	"use strict";
 
-// Preloader
-$(window).on('load', function () {
-	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
-	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(333);
-});
+
 
 
 // Header Sticky
